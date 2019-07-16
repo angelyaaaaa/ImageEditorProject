96875523ec3ef83d6f2f5bddf75645e67fa1d894
angelyaaaaa <tsoen.526018@gmail.com>
2019-07-16 16:56:08 +0800

    layout setting

---
+++ src/components/DisplayArea.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './DisplayArea.css';
+
+const DisplayArea = () => {
+    return (
+        <div className="display-area">
+            display area
+        </div>
+    );
+}
+
+export default DisplayArea;
---
+++ src/components/UploadArea.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './UploadArea.css';
+
+const UploadArea = () => {
+    return (
+        <div className="upload-area">
+            <div className="upload-header">
+                <div className="upload-user"></div>
+            </div>
+            <div className="upload-image">
+            </div>
+        </div>
+    );
+}
+
+export default UploadArea;
